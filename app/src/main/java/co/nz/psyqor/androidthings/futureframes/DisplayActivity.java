c4884e33ed706656360afce87164c2ad2ceeb9cb
Mitchell Lane <mitchell.lane52@gmail.com>
2017-01-15 21:28:26 +1300

    Use the AutoScrollViewPager library for timed slideshow scrolling

--- app/src/main/java/co/nz/psyqor/androidthings/futureframes/DisplayActivity.java
+++ app/src/main/java/co/nz/psyqor/androidthings/futureframes/DisplayActivity.java
@@ -29,6 +29,8 @@ import android.support.v4.app.FragmentManager;
 
 import com.google.android.things.pio.PeripheralManagerService;
 
+import cn.trinea.android.view.autoscrollviewpager.AutoScrollViewPager;
+
 /**
  * Displays a repeating slideshow of photos
  */
@@ -39,7 +41,10 @@ public class DisplayActivity extends FragmentActivity {
     private PeripheralManagerService pioService = new PeripheralManagerService();
 
     private static final int NUM_PAGES = 5;
-    private ViewPager mPager;
+    private static final int SCROLL_INTERVAL = 3000;
+    private static final int SCROLL_DURATION = 5;
+
+    private AutoScrollViewPager mPager;
     private PagerAdapter mPagerAdapter;
 
     @Override
@@ -48,9 +53,32 @@ public class DisplayActivity extends FragmentActivity {
         Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_display);
 
-        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager = (AutoScrollViewPager) findViewById(R.id.pager);
         mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
         mPager.setAdapter(mPagerAdapter);
+
+        configurePager();
+    }
+
+    public void configurePager(){
+        mPager.setInterval(SCROLL_INTERVAL);
+        mPager.setScrollDurationFactor(SCROLL_DURATION);
+        mPager.setSlideBorderMode(AutoScrollViewPager.SLIDE_BORDER_MODE_CYCLE); // Supposed to scroll at start/end but is buggy
+        mPager.startAutoScroll();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // stop auto scroll when onPause
+        mPager.stopAutoScroll();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // start auto scroll when onResume
+        mPager.startAutoScroll();
     }
 
     @Override
--- app/src/main/java/co/nz/psyqor/androidthings/futureframes/ScreenSlidePageFragment.java
+++ app/src/main/java/co/nz/psyqor/androidthings/futureframes/ScreenSlidePageFragment.java
@@ -19,12 +19,7 @@ import static android.R.attr.path;
 import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * { ScreenSlidePageFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ScreenSlidePageFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * A simple {@link Fragment} subclass that holds an image that can be swiped left or right for the next one
  */
 public class ScreenSlidePageFragment extends Fragment {
     private static final String TAG = ScreenSlidePageFragment.class.getSimpleName();
@@ -34,43 +29,17 @@ public class ScreenSlidePageFragment extends Fragment {
     private int mItemNumber;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(
-                R.layout.fragment_screen_slide_page, container, false);
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_screen_slide_page, container, false);
         ImageView imageView = (ImageView) rootView.findViewById(R.id.slide_image);
         setImage(imageView, mItemNumber);
-//        Drawable myDrawable = getResources().getDrawable(R.drawable.img_2016_0);
-//        imageView.setImageDrawable(myDrawable);
-//        imageView.setImageDrawable(R.drawable.img_2016_0);
         return rootView;
-    }
-
-    // Sets the image at position to the given imageView
-    public void setImage(ImageView imageView, int position) {
-        String path = "img_2016_" + position;
-        Log.d(TAG, "Loading image at path: "+ path + " from package " + MyApp.getContext().getPackageName());
-
-//            R.drawable.img_2016_0
-        int imageResource = getResources().getIdentifier(path, "drawable", MyApp.getContext().getPackageName());
-        Log.d(TAG, "loading image with resource: "+imageResource);
-        imageView.setImageResource(imageResource);
-//        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), imageResource);
-//        imageView.setImageBitmap(bitmap);
     }
 
     public ScreenSlidePageFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param itemNumber Parameter 1.
-     * @return A new instance of fragment ScreenSlidePageFragment.
-     */
     public static ScreenSlidePageFragment newInstance(int itemNumber) {
         ScreenSlidePageFragment fragment = new ScreenSlidePageFragment();
         Bundle args = new Bundle();
@@ -87,45 +56,13 @@ public class ScreenSlidePageFragment extends Fragment {
         }
     }
 
-//    private OnFragmentInteractionListener mListener;
-//    }
-//
-//    // TODO: Rename method, update argument and hook method into UI event
-//    public void onButtonPressed(Uri uri) {
-//        if (mListener != null) {
-//            mListener.onFragmentInteraction(uri);
-//        }
-//    }
-//
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-//    }
-//
-//    @Override
-//    public void onDetach() {
-//        super.onDetach();
-//        mListener = null;
-//    }
-//
-//    /**
-//     * This interface must be implemented by activities that contain this
-//     * fragment to allow an interaction in this fragment to be communicated
-//     * to the activity and potentially other fragments contained in that
-//     * activity.
-//     * <p>
-//     * See the Android Training lesson <a href=
-//     * "http://developer.android.com/training/basics/fragments/communicating.html"
-//     * >Communicating with Other Fragments</a> for more information.
-//     */
-//    public interface OnFragmentInteractionListener {
-//        // TODO: Update argument type and name
-//        void onFragmentInteraction(Uri uri);
-//    }
+    // Sets the image at position to the given imageView
+    private void setImage(ImageView imageView, int position) {
+        String path = "img_2016_" + position;
+        Log.d(TAG, "Loading image at path: "+ path + " from package " + MyApp.getContext().getPackageName());
+        int imageResource = getResources().getIdentifier(path, "drawable", MyApp.getContext().getPackageName());
+        imageView.setImageResource(imageResource);
+    }
+
+
 }
